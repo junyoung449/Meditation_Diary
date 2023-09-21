89cb088bf91101102295a4aa5a0c3e2ea2022b79
JNH <math_star@naver.com>
2023-09-21 13:33:24 +0900

    [feat] 카카오 로그인시 토큰 저장

--- front/src/index.js
+++ front/src/index.js
@@ -29,7 +29,7 @@ import "@fortawesome/fontawesome-free/css/all.min.css";
 
 import ThemeContextWrapper from "./components/ThemeWrapper/ThemeWrapper";
 import BackgroundColorWrapper from "./components/BackgroundColorWrapper/BackgroundColorWrapper";
-import KakaoLogined from "components/Kakao/Kakao";
+import KakaoLogined from "./components/Kakao/Kakao";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
