9d39c0b0ec9d0d121de030b1ec27f620a39dbd8a
JNH <math_star@naver.com>
2023-10-04 09:41:52 +0900

    [Feat] FixedPlugin 버튼 변경

--- front/src/layouts/CalendarAndMemoAndFeed/Footer.jsx
+++ front/src/layouts/CalendarAndMemoAndFeed/Footer.jsx
@@ -1,9 +1,16 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { ThemeContext } from 'contexts/ThemeContext';
 import { useNavigate } from 'react-router-dom';
 import '../../assets/css/Footer.css';
 
 function Footer({ setSelectedTab }) {
   const navigate = useNavigate();
+  const { theme, changeTheme } = useContext(ThemeContext);
+
+  const toggleTheme = () => {
+    // 테마 변경 함수 호출
+    changeTheme(); // 이 함수를 호출하면 테마가 변경됩니다.
+  };
 
   const handleCreateClick = () => {
     navigate("/crud");
@@ -15,7 +22,7 @@ function Footer({ setSelectedTab }) {
 
   return (
     <div className="footer">
-      <button className="footer-btn btn-left">
+      <button className="footer-btn btn-left" onClick={toggleTheme}>
         <img src="https://s3.ap-northeast-2.amazonaws.com/b205.s3test.bucket/footer/footer-left.png"/>
       </button>
       <button className="footer-btn btn-center" onClick={handleCreateClick}>
