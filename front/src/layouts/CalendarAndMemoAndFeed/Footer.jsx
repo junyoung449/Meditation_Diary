27661989f61bf04d196a277486d83dd8b91f67c4
JNH <math_star@naver.com>
2023-10-05 21:31:01 +0900

    [Css] Footer 흑백 테마별 처리

--- front/src/layouts/CalendarAndMemoAndFeed/Footer.jsx
+++ front/src/layouts/CalendarAndMemoAndFeed/Footer.jsx
@@ -10,7 +10,7 @@ function Footer({ setSelectedTab }) {
   const toggleTheme = () => {
     // 현재 테마가 "light"이면 "dark"로, 그 반대면 "light"로 변경
     const newTheme = theme === themes.light ? themes.dark : themes.light;
-    console.log(newTheme,'으로 배경색 변경')
+    console.log(newTheme, '으로 배경색 변경')
     changeTheme(newTheme);
   };
 
@@ -18,21 +18,24 @@ function Footer({ setSelectedTab }) {
     navigate("/crud");
   };
 
-  const handleFeedClick = () => { // 오른쪽 버튼 클릭 핸들러를 추가합니다.
+  const handleFeedClick = () => {
     setSelectedTab("Feed");
     navigate("/frame")
   };
 
+  // 테마에 따라 클래스를 동적으로 설정
+  const footerClass = theme === themes.light ? '' : 'dark-theme';
+
   return (
-    <div className="footer">
+    <div className={`footer ${footerClass}`}>
       <button className="footer-btn btn-left" onClick={toggleTheme}>
-        <img src="https://s3.ap-northeast-2.amazonaws.com/b205.s3test.bucket/footer/footer-left.png"/>
+        <img src="https://s3.ap-northeast-2.amazonaws.com/b205.s3test.bucket/footer/footer-left.png" />
       </button>
       <button className="footer-btn btn-center" onClick={handleCreateClick}>
-        <img src="https://s3.ap-northeast-2.amazonaws.com/b205.s3test.bucket/footer/footer-center.png"/>
+        <img src="https://s3.ap-northeast-2.amazonaws.com/b205.s3test.bucket/footer/footer-center.png" />
       </button>
       <button className="footer-btn btn-right" onClick={handleFeedClick}>
-        <img src="https://s3.ap-northeast-2.amazonaws.com/b205.s3test.bucket/footer/footer-right.png"/>
+        <img src="https://s3.ap-northeast-2.amazonaws.com/b205.s3test.bucket/footer/footer-right.png" />
       </button>
     </div>
   );
