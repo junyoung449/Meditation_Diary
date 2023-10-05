58eb8f47e684478e85a5b33e2a85de095bdab388
hyungseok Lee <83962223+gudtjr2949@users.noreply.github.com>
2023-10-06 00:34:12 +0900

    [Feat] 백엔드 서버시간 설정 231006 0034

--- mt/src/main/java/com/project/mt/MtApplication.java
+++ mt/src/main/java/com/project/mt/MtApplication.java
@@ -3,11 +3,19 @@ package com.project.mt;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import javax.annotation.PostConstruct;
+import java.util.TimeZone;
+
 @SpringBootApplication
 public class MtApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MtApplication.class, args);
+	}
+
+	@PostConstruct
+	void started(){
+		TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
 	}
 
 }
