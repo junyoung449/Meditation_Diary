dda6abeec7f6d30912301e36c9728018a7160e1f
이형석 <gudtjr2949@naver.com>
2023-10-06 00:14:48 +0900

    [fix]볼륨 조절

--- fastApiProject/main.py
+++ fastApiProject/main.py
@@ -272,7 +272,7 @@ def makeBackGroundMusic(name):
     background_music = AudioSegment.from_mp3("./audio/back.mp3")
 
     # 원본 mp3 파일 로드
-    original_audio = AudioSegment.from_mp3("./audio/" + name + ".mp3") - 10
+    original_audio = AudioSegment.from_mp3("./audio/" + name + ".mp3") - 2
 
     # 로컬에 있는 mp3 파일 삭제
     os.remove("./audio/" + name + ".mp3")
